3a8f362959c0a241bb25b5eb3aa8f7ccea52a900
Kushnir Yuriy <kushnir.yb@gmail.com>
2017-09-06 15:21:05 +0300

    Adds session_age & session_id input parameters

--- lib/minfraud/components/device.rb
+++ lib/minfraud/components/device.rb
@@ -14,6 +14,15 @@ module Minfraud
       # @return [String] The HTTP "Accept-Language" header of the browser used in the transaction
       attr_accessor :accept_language
 
+      # @attribute :session_age
+      # @return [Decimal] The number of seconds between the creation of the user’s session and the time of the transaction.
+      # Note that session_age is not the duration of the current visit, but the time since the start of the first visit.
+      attr_accessor :session_age
+
+      # @attribute :session_id
+      # @return [String] An ID that uniquely identifies a visitor’s session on the site.
+      attr_accessor :session_id
+
       # Creates Minfraud::Components::Device instance
       # @param  [Hash] params hash of parameters
       # @return [Minfraud::Components::Device] a Device instance
@@ -21,6 +30,8 @@ module Minfraud
         @ip_address      = params[:ip_address]
         @user_agent      = params[:user_agent]
         @accept_language = params[:accept_language]
+        @session_age     = params[:session_age]
+        @session_id      = params[:session_id]
       end
     end
   end
