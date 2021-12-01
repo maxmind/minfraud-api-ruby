60cad5f43ac69ca88864f75e2a72217b6bd399a4
William Storey <wstorey@maxmind.com>
2021-12-01 09:10:29 -0800

    Add end to end HTTP test for report transaction

--- spec/http_spec.rb
+++ spec/http_spec.rb
@@ -63,3 +63,54 @@ describe Minfraud::Assessments do
     expect { assessment.score }.to raise_error(Minfraud::AuthorizationError)
   end
 end
+
+describe Minfraud::Report do
+  it 'makes a successful HTTP request' do
+    stub_request(:post, /report/).with(
+      body:    '{"ip_address":"1.2.3.4"}',
+      headers: {
+        'Authorization' => 'Basic MTE6bXlfbGljZW5zZV9rZXk=',
+      },
+    ).to_return(
+      status: 204,
+    )
+
+    Minfraud.configure do |c|
+      c.account_id        = 11
+      c.license_key       = 'my_license_key'
+      c.enable_validation = true
+    end
+
+    txn      = Minfraud::Components::Report::Transaction.new(
+      ip_address: '1.2.3.4',
+    )
+    reporter = Minfraud::Report.new(transaction: txn)
+
+    expect(reporter.report_transaction).to eq nil
+  end
+
+  it 'makes an HTTP request resulting in an HTTP 401' do
+    stub_request(:post, /report/).with(
+      body:    '{"ip_address":"1.2.3.4"}',
+    ).to_return(
+      body:    '{"code":"ACCOUNT_ID_REQUIRED","error":"You have not supplied a MaxMind account ID in the Authorization header."}',
+      headers: {
+        'Content-Type' => 'application/vnd.maxmind.com-error+json; charset=UTF-8; version=2.0',
+      },
+      status:  401,
+    )
+
+    Minfraud.configure do |c|
+      c.account_id        = 11
+      c.license_key       = 'my_license_key'
+      c.enable_validation = false
+    end
+
+    txn      = Minfraud::Components::Report::Transaction.new(
+      ip_address: '1.2.3.4',
+    )
+    reporter = Minfraud::Report.new(transaction: txn)
+
+    expect { reporter.report_transaction }.to raise_error(Minfraud::AuthorizationError)
+  end
+end
